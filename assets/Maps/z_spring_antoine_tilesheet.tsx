f93b6cd2f48c5709f533544ef760dc9dd063e712
Berry <serenityfrost42@gmail.com>
2025-05-18 08:45:59 -0500

    Art and Maps

---
+++ assets/Maps/z_spring_antoine_tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="z_spring_antoine_tilesheet" tilewidth="16" tileheight="16" tilecount="1008" columns="36">
+ <image source="z_spring_antoine_tilesheet.png" width="580" height="462"/>
+</tileset>
