797a4c8e56b1a85e5a7f4b0ee537791ff67e949b
Berry <serenityfrost42@gmail.com>
2025-05-21 18:03:58 -0500

    Art Refresh
    More new assets, cleaned up old busted assets. Take two on Antoine's Interior map.

--- assets/Maps/z_spring_antoine_tilesheet.tsx
+++ assets/Maps/z_spring_antoine_tilesheet.tsx
@@ -1,4 +1,116 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="z_spring_antoine_tilesheet" tilewidth="16" tileheight="16" tilecount="1008" columns="36">
- <image source="z_spring_antoine_tilesheet.png" width="580" height="462"/>
+<tileset version="1.10" tiledversion="1.11.2" name="z_spring_antoine_tilesheet" tilewidth="16" tileheight="16" tilecount="1908" columns="36">
+ <image source="z_spring_antoine_tilesheet.png" width="576" height="848"/>
+ <tile id="216">
+  <animation>
+   <frame tileid="216" duration="180"/>
+   <frame tileid="217" duration="180"/>
+   <frame tileid="218" duration="180"/>
+   <frame tileid="219" duration="180"/>
+  </animation>
+ </tile>
+ <tile id="613">
+  <animation>
+   <frame tileid="613" duration="600"/>
+   <frame tileid="615" duration="1200"/>
+  </animation>
+ </tile>
+ <tile id="620">
+  <animation>
+   <frame tileid="620" duration="150"/>
+   <frame tileid="621" duration="150"/>
+   <frame tileid="622" duration="150"/>
+   <frame tileid="623" duration="150"/>
+   <frame tileid="622" duration="150"/>
+   <frame tileid="621" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="656">
+  <animation>
+   <frame tileid="656" duration="150"/>
+   <frame tileid="657" duration="150"/>
+   <frame tileid="658" duration="150"/>
+   <frame tileid="659" duration="150"/>
+   <frame tileid="658" duration="150"/>
+   <frame tileid="657" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="660">
+  <animation>
+   <frame tileid="660" duration="150"/>
+   <frame tileid="662" duration="150"/>
+   <frame tileid="592" duration="150"/>
+   <frame tileid="590" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="661">
+  <animation>
+   <frame tileid="661" duration="150"/>
+   <frame tileid="663" duration="150"/>
+   <frame tileid="593" duration="150"/>
+   <frame tileid="591" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="684">
+  <animation>
+   <frame tileid="684" duration="100"/>
+   <frame tileid="685" duration="100"/>
+   <frame tileid="686" duration="100"/>
+   <frame tileid="687" duration="100"/>
+   <frame tileid="688" duration="100"/>
+   <frame tileid="689" duration="100"/>
+   <frame tileid="690" duration="100"/>
+   <frame tileid="691" duration="100"/>
+   <frame tileid="690" duration="100"/>
+   <frame tileid="689" duration="100"/>
+   <frame tileid="688" duration="100"/>
+   <frame tileid="687" duration="100"/>
+   <frame tileid="686" duration="100"/>
+   <frame tileid="685" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="696">
+  <animation>
+   <frame tileid="696" duration="150"/>
+   <frame tileid="698" duration="150"/>
+   <frame tileid="628" duration="150"/>
+   <frame tileid="626" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="697">
+  <animation>
+   <frame tileid="697" duration="150"/>
+   <frame tileid="699" duration="150"/>
+   <frame tileid="629" duration="150"/>
+   <frame tileid="627" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="720">
+  <animation>
+   <frame tileid="720" duration="100"/>
+   <frame tileid="721" duration="100"/>
+   <frame tileid="722" duration="100"/>
+   <frame tileid="723" duration="100"/>
+   <frame tileid="724" duration="100"/>
+   <frame tileid="725" duration="100"/>
+   <frame tileid="726" duration="100"/>
+   <frame tileid="727" duration="100"/>
+   <frame tileid="726" duration="100"/>
+   <frame tileid="725" duration="100"/>
+   <frame tileid="724" duration="100"/>
+   <frame tileid="723" duration="100"/>
+   <frame tileid="722" duration="100"/>
+   <frame tileid="721" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="764">
+  <animation>
+   <frame tileid="764" duration="150"/>
+   <frame tileid="765" duration="150"/>
+   <frame tileid="766" duration="150"/>
+   <frame tileid="767" duration="150"/>
+   <frame tileid="766" duration="150"/>
+   <frame tileid="765" duration="150"/>
+  </animation>
+ </tile>
 </tileset>
